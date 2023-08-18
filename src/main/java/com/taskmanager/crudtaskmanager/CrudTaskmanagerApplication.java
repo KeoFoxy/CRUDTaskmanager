a6fa685f3f5ac8ce3a6d113ccdbb7c6403f850aa
KeoFoxy <arasdiel68@gmail.com>
2023-08-18 15:10:46 +0300

    Main

--- src/main/java/com/taskmanager/crudtaskmanager/CrudTaskmanagerApplication.java
+++ src/main/java/com/taskmanager/crudtaskmanager/CrudTaskmanagerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrudTaskmanagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrudTaskmanagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CrudTaskmanagerApplication.class, args);
+    }
 
 }
