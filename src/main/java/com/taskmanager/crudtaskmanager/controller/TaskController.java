4153c0db635b332ac4041eda94aee6f78059db8d
KeoFoxy <arasdiel68@gmail.com>
2023-08-18 15:10:54 +0300

    Task Controller

--- src/main/java/com/taskmanager/crudtaskmanager/controller/TaskController.java
+++ src/main/java/com/taskmanager/crudtaskmanager/controller/TaskController.java
@@ -1,2 +1,38 @@
-package com.taskmanager.crudtaskmanager.controller;public class TaskController {
+package com.taskmanager.crudtaskmanager.controller;
+
+import com.taskmanager.crudtaskmanager.dto.TaskDto;
+import com.taskmanager.crudtaskmanager.entity.Task;
+import com.taskmanager.crudtaskmanager.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Task> createTask(@RequestBody TaskDto taskDto) {
+        Task task = taskService.createTask(taskDto.getTitle(), taskDto.isCompleted());
+        return ResponseEntity.ok(task);
+    }
+
+//    public Task updateTask(Task taskDto, Long taskId) {
+//
+//    }
+//
+//    public Task getTaskById(Long id) {
+//
+//    }
+//
+//    public void deleteTaskById(Long id) {
+//
+//    }
 }
