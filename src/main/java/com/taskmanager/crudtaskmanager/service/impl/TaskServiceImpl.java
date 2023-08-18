d8ba4ede18deda7433ab9f2a6a1f4a2475be41d2
KeoFoxy <arasdiel68@gmail.com>
2023-08-18 15:11:26 +0300

    Task Service Impl

--- src/main/java/com/taskmanager/crudtaskmanager/service/impl/TaskServiceImpl.java
+++ src/main/java/com/taskmanager/crudtaskmanager/service/impl/TaskServiceImpl.java
@@ -1,2 +1,29 @@
-package com.taskmanager.crudtaskmanager.service.impl;public class TaskServiceImpl {
+package com.taskmanager.crudtaskmanager.service.impl;
+
+import com.taskmanager.crudtaskmanager.TaskRepository.TaskRepository;
+import com.taskmanager.crudtaskmanager.entity.Task;
+import com.taskmanager.crudtaskmanager.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+    private TaskRepository taskRepository;
+
+    @Autowired
+    public TaskRepository getTaskRepository(TaskRepository taskRepository) {
+        return taskRepository;
+    }
+
+    @Override
+    public Task createTask(String title, boolean isCompleted) {
+        Task task = new Task();
+        task.setCompleted(isCompleted);
+        task.setTitle(title);
+        task.setCreationDate(LocalDate.now());
+
+        return taskRepository.save(task);
+    }
 }
