d11400326ebd1b310d2112c3572926011ff3ebcb
KeoFoxy <arasdiel68@gmail.com>
2023-08-18 15:11:06 +0300

    Task Entity

--- src/main/java/com/taskmanager/crudtaskmanager/entity/Task.java
+++ src/main/java/com/taskmanager/crudtaskmanager/entity/Task.java
@@ -1,2 +1,25 @@
-package com.taskmanager.crudtaskmanager.entity;public class Task {
+package com.taskmanager.crudtaskmanager.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private boolean isCompleted;
+    private LocalDate creationDate;
+    private String title;
 }
