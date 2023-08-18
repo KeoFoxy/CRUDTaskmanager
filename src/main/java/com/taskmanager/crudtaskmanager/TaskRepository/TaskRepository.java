86f9b1c84469042af19f58787ac877134c06e533
KeoFoxy <arasdiel68@gmail.com>
2023-08-18 15:29:04 +0300

    Task Repostiroy now has @Repository

--- src/main/java/com/taskmanager/crudtaskmanager/TaskRepository/TaskRepository.java
+++ src/main/java/com/taskmanager/crudtaskmanager/TaskRepository/TaskRepository.java
@@ -2,6 +2,8 @@ package com.taskmanager.crudtaskmanager.TaskRepository;
 
 import com.taskmanager.crudtaskmanager.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
